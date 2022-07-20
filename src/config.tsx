6143ca0af550738dc68316612875bd4f46bbffce
Sebastian Dancu <sebidancu98@gmail.com>
2022-07-20 19:11:49 +0300

    mainnet

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqhnn484f5ep20c7xlqa9c5hvawfc9lz5yj0wq6kshla';
+  'erd1qqqqqqqqqqqqqpgq8txxlht84uks5hedzsk0pd2g32c59536j0wqqrn74l';
 
 export const dAppName = 'TIGER\'S PARTY CLUB';
-export const mode = 'devnet-';
-export const nft1= 'ULBS-c22d32';
-export const nft2='FACES-dd0aec';
+export const mode = '';
+export const nft1= 'TPCB-8ed04a';
+export const nft2='TPCE-559720';
